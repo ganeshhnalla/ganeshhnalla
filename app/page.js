967f8594971a991438477c4135b8f058634d2a5a
Ganesh Nalla <ganeshhnalla@aol.com>
2024-12-10 16:59:46 +0530

    Update webpage

--- app/page.js
+++ app/page.js
@@ -2,100 +2,92 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+    <main className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen duration-300 p-8 pb-20 gap-16 sm:p-20">
+      <div className="flex flex-col gap-8 row-start-2 duration-300 items-center md:items-start">
+        <div className="bg-[#ccc] w-full border-t border-black"></div>
+        <p className="text-2xl font-bold duration-300">Ganesh Nalla</p>
+        <div className="bg-[#ccc] w-full border-t border-black"></div>
+        <ol className="list-inside text-base">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
+            A final year student specializing in Artificial Intelligence at
+            Bennett University. Passionate about leveraging machine learning and
+            deep learning techniques to solve real-world problems. Seeking
+            opportunities to apply my AI skills in a challenging and
+            growth-oriented environment. Feel free to explore and reach out if
+            you’d like to collaborate!
           </li>
-          <li>Save and see your changes instantly.</li>
         </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="bg-[#ccc] w-full border-t border-black"></div>
+        <ol className="list-inside text-base">
+          <li>
+            <b>Programming Languages:</b> Python, C++, Java, Javascript
+          </li>
+          <li>
+            <b>Frameworks:</b> PyTorch, Scikit-Learn, NextJS, React, Pandas,
+            NumPy, Matplotlib
+          </li>
+          <li>
+            <b>Tools:</b> Jupyter, Git, SQL, Docker, Figma, Postman, MySQL,
+            MongoDB
+          </li>
+          <li>
+            <b>Other Skills:</b> Problem-solving, Agile methodologies,
+            Collaboration, Communication
+          </li>
+        </ol>
+        <div className="bg-[#ccc] w-full border-t border-black"></div>
+        <ol className="list-inside text-base">
+          <li className="text-lg mb-2">
+            <b>Latest Project</b>
+          </li>
+          <li>
+            <a
+              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+              href="https://ganeshhnalla.github.io/caferukma"
+            >
+              <b>CafeRukma →</b>
+            </a>
+          </li>
+          <li>
+            Redesigned website with server side rendering and new gallery view
+            of the cafe from a old chunky design.
+          </li>
+        </ol>
+        <div className="flex mt-2 gap-4">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="rounded-full border border-solid border-[#ccc] transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-gray-300 text-sm h-12 px-5 w-32"
+            href="https://drive.google.com/file/d/1fWyhrccM9kytUcNno6k2RYungZfZRDzN/view?usp=sharing"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            Resume →
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="rounded-full border border-solid border-[#ccc] transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-gray-300 text-sm h-10 sm:h-12 px-4 sm:px-5 w-32"
+            href="https://medium.com/@escotera"
           >
-            Read our docs
+            Blog →
           </a>
         </div>
-      </main>
+      </div>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="mailto:ganeshhnalla@aol.com"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          Contact →
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://github.com/ganeshhnalla"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          Github →
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://medium.com/@escotera/subscribe"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Subscribe to my newsletter →
         </a>
       </footer>
-    </div>
+    </main>
   );
 }
